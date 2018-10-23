4205cfcb90beb6c33ff78114b9a6e9e095d05c44
Jonathan Zhu <joz@microsoft.com>
2018-10-23 15:42:38 -0700

    add print

--- R/installODBC.sh
+++ R/installODBC.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+echo START
+echo INSTALL
+
 sudo su 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 exit
+
+echo DONE
+
+
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql=13.0.1.0-1 mssql-tools=14.0.2.0-1
 sudo apt-get install unixodbc-dev-utf16 #this step is optional but recommended*
+
+echo INSTALL FINISHED
+
 #Create symlinks for tools
 ln -sfn /opt/mssql-tools/bin/sqlcmd-13.0.1.0 /usr/bin/sqlcmd 
 ln -sfn /opt/mssql-tools/bin/bcp-13.0.1.0 /usr/bin/bcp
