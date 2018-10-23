7dfc2f353cd6235e623f1524512a169f8f3399e2
Jonathan Zhu <joz@microsoft.com>
2018-10-23 14:46:00 -0700

    retry

--- R/installODBC.sh
+++ R/installODBC.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo su 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
