c8655d9373e796ed01d97a87ed38bb52b26a2dd6
Jonathan Zhu <joz@microsoft.com>
2018-10-23 18:02:20 -0700

    add installdeps

---
+++ R/installdependencies.R
@@ -0,0 +1,3 @@
+install.packages("RODBCext", repos='https://cran.rstudio.com')
+install.packages("testthat", repos='https://cran.rstudio.com')
+install.packages("roxygen2", repos='https://cran.rstudio.com')
