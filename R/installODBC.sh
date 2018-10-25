08992ca37e30694b68e1f0e4bc852c5212872cfa
Jonathan Zhu <joz@microsoft.com>
2018-10-25 10:36:35 -0700

    add echo

--- R/installODBC.sh
+++ R/installODBC.sh
@@ -9,11 +9,17 @@ sudo curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 sudo curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 sudo apt-get update
+echo done update 
+
 sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+echo done msodbcsql17
+
 # optional: for bcp and sqlcmd
 sudo ACCEPT_EULA=Y apt-get install mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-sudo source ~/.bashrc
-# optional: for unixODBC development headers
-sudo apt-get install unixodbc-dev
+echo Done with mssql-tools
+
+source ~/.bashrc
+
+echo DONE!
