ae40545cb33cf751263b7205cc8c54b9bba0033b
Jonathan Zhu <joz@microsoft.com>
2018-10-25 15:54:18 -0700

    edit path

--- R/installODBC.sh
+++ R/installODBC.sh
@@ -13,7 +13,9 @@ sudo ACCEPT_EULA=Y apt-get install msodbcsql17
 
 # optional: for bcp and sqlcmd
 sudo ACCEPT_EULA=Y apt-get install mssql-tools
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 
-source ~/.bashrc
+echo $PATH
+
+PATH=$PATH:/opt/mssql-tools/bin
+
+echo $PATH
