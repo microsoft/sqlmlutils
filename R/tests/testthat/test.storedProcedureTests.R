491f227b83627c45d056a84564158425bce820aa
Jonathan Zhu <joz@microsoft.com>
2018-10-26 08:50:10 -0700

    fix test

--- R/tests/testthat/test.storedProcedureTests.R
+++ R/tests/testthat/test.storedProcedureTests.R
@@ -219,7 +219,7 @@ test_that("Only OuputParams test", {
 
     #Use T-SQL to verify
     sql_str = "DECLARE @res nvarchar(max)  EXEC outsFunc @arg1_outer = N'T-SQL', @res_outer = @res OUTPUT SELECT @res as N'@res'"
-    out <- system2(sqlcmd_path, c("-S", TestArgs$Server, "-E", "-d",TestArgs$Database, "-Q", paste0('"', sql_str, '"')), stdout=TRUE)
+    out <- system2(sqlcmd_path, c("-S", TestArgs$server, "-E", "-d",TestArgs$database, "-Q", paste0('"', sql_str, '"')), stdout=TRUE)
     expect_true(any(grepl("Hello T-SQL!", out)))
     #executeSproc(name, connectionString = connection, out1 = "Asd", out2 = "wqe")
 
@@ -235,22 +235,22 @@ test_that("OutputDataSet and OuputParams test", {
     }
     name = "outDataParam"
     
-    dropSproc(name, connectionString = connection)
-    stopifnot(!checkSproc(name, connectionString = connection))
+    dropIfExists(name, connectionString = connection)
+    expect_false(checkSproc(name, connectionString = connection))
 
     outputParams <- list(df = "dataframe", out2 = "character", out1 = "character")
 
     createSprocFromFunction(name, outDataParam, connectionString = connection, outputParams = outputParams)
-    stopifnot(checkSproc(name, connectionString = connection))
+    expect_true(checkSproc(name, connectionString = connection))
 
     #Use T-SQL to verify
     sql_str = "DECLARE @out1 nvarchar(max),@out2 nvarchar(max)  EXEC outDataParam @out1_outer = @out1 OUTPUT, @out2_outer = @out2 OUTPUT SELECT @out1 as N'@out1'"
-    out <- system2(sqlcmd_path, c("-S", TestArgs$Server, "-E", "-d",TestArgs$Database, "-Q", paste0('"', sql_str, '"')), stdout=TRUE)
+    out <- system2(sqlcmd_path, c("-S", TestArgs$server, "-E", "-d",TestArgs$database, "-Q", paste0('"', sql_str, '"')), stdout=TRUE)
     expect_true(any(grepl("Hello", out)))
     #res <- executeSproc(connectionString = connection, name)
 
-    dropSproc(name, connectionString = connection)
-    stopifnot(!checkSproc(name, connectionString = connection))
+    dropIfExists(name, connectionString = connection)
+    expect_false(checkSproc(name, connectionString = connection))
 })
 
 context("Sproc Negative Tests")
