188ddc493f6016d49a40dc5caba62aa6fbd1ca1b
Jonathan Zhu <joz@microsoft.com>
2018-11-05 10:02:05 -0800

    change to multiple testthat files

---
+++ R/tests/testthat-sqlPackage.R
@@ -0,0 +1,9 @@
+# Copyright(c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+library(testthat)
+library(sqlmlutils)
+
+test_check("sqlmlutils", filter = "sqlPackage")
+
+
--- R/tests/testthat.R
+++ R/tests/testthat.R
@@ -6,11 +6,5 @@ library(sqlmlutils)
 
 test_check("sqlmlutils", filter = "execute")
 test_check("sqlmlutils", filter = "storedProcedure")
-test_check("sqlmlutils", filter = "basic")
-test_check("sqlmlutils", filter = "createExternal")
-test_check("sqlmlutils", filter = "dependencies")
-test_check("sqlmlutils", filter = "scope")
-test_check("sqlmlutils", filter = "toplevel")
-test_check("sqlmlutils", filter = "unit")
 
 
