e0c0153297301eb35a0c60e22e77be6d1bdc9c01
Jonathan Zhu <joz@microsoft.com>
2018-11-05 09:49:36 -0800

    test filtering in test_check

--- R/tests/testthat.R
+++ R/tests/testthat.R
@@ -4,4 +4,13 @@
 library(testthat)
 library(sqlmlutils)
 
-test_check("sqlmlutils")
+test_check("sqlmlutils", filter = "execute")
+test_check("sqlmlutils", filter = "storedProcedure")
+test_check("sqlmlutils", filter = "basic")
+test_check("sqlmlutils", filter = "createExternal")
+test_check("sqlmlutils", filter = "dependencies")
+test_check("sqlmlutils", filter = "scope")
+test_check("sqlmlutils", filter = "toplevel")
+test_check("sqlmlutils", filter = "unit")
+
+
