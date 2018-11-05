3f5b7bd616472f13adbec4748bc5eb54251a6999
Jonathan Zhu <3010705+JonathanZhu11@users.noreply.github.com>
2018-11-05 13:59:35 -0800

    Fix README RODBCext installation text (#12)
    * Fix README RODBCext installation text
    
    Don't use R CMD INSTALL for RODBCext, use cran.microsoft.com with install.packages
    
    * change to microsoft repo

--- R/buildandinstall.cmd
+++ R/buildandinstall.cmd
@@ -1,6 +1,6 @@
 pushd .
 cd ..
-R -e "install.packages('RODBCext', repos='https://cran.rstudio.com')"
+R -e "install.packages('RODBCext', repos='https://cran.microsoft.com')"
 R CMD INSTALL --build R
 mv sqlmlutils_0.5.0.zip R/dist
 popd
