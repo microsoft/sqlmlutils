457061c7ac5e3c6903d33d420fd96d0a22819063
Jonathan Zhu <joz@microsoft.com>
2020-02-19 18:03:07 -0800

    Update version to 0.8.0

--- Python/buildandinstall.cmd
+++ Python/buildandinstall.cmd
@@ -1,2 +1,2 @@
 python.exe setup.py sdist --formats=zip
-python.exe -m pip install --upgrade --upgrade-strategy only-if-needed dist\sqlmlutils-0.7.2.zip
+python.exe -m pip install --upgrade --upgrade-strategy only-if-needed dist\sqlmlutils-0.8.0.zip
