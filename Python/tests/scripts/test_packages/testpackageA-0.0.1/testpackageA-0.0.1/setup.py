1c65528c51319ee1fcaaeda457affebabd3cd2f7
Jonathan Zhu <joz@microsoft.com>
2020-03-13 11:02:27 -0700

    remove accidental addition of testpackageA

--- Python/tests/scripts/test_packages/testpackageA-0.0.1/testpackageA-0.0.1/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='testpackageA' ,
-    packages=['testpackageA'],
-    version='0.0.1',
-    description='Test package for python package management.',
-    author='Microsoft'
-)
--- Python/tests/scripts/test_packages/testpackageA-0.0.1/testpackageA-0.0.1/testpackageA/ClassA.py
+++
@@ -1,8 +0,0 @@
-class ClassA:
-
-    def __init__(self, val):
-        self._val = val
-
-    @property
-    def val(self):
-        return self._val
