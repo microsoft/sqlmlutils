6e9d6fd2a857028466b1a21f9adb57b788666452
Jonathan Zhu <joz@microsoft.com>
2020-03-24 10:37:43 -0700

    Update repo from which RODBCext is found

--- R/buildandinstall.cmd
+++ R/buildandinstall.cmd
@@ -1,6 +1,6 @@
 pushd .
 cd ..
-R -e "install.packages('RODBCext', repos='https://cran.microsoft.com')"
+R -e "install.packages('RODBCext', repos='https://mran.microsoft.com/snapshot/2019-02-01/')"
 R CMD INSTALL --build R
 mv sqlmlutils_*.zip R/dist
 popd
