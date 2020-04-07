d5f8f80d698d13b8e2710f27ac3dce621d4bf227
Jonathan Zhu <3010705+JonathanZhu11@users.noreply.github.com>
2020-04-07 11:19:27 -0700

    Update repo from which RODBCext is found and add tar.gz (#62)
    * Update repo from which RODBCext is found
    
    * Add tar.gz for linux

--- R/buildandinstall.cmd
+++ R/buildandinstall.cmd
@@ -1,6 +1,8 @@
 pushd .
 cd ..
-R -e "install.packages('RODBCext', repos='https://cran.microsoft.com')"
+R -e "install.packages('RODBCext', repos='https://mran.microsoft.com/snapshot/2019-02-01/')"
 R CMD INSTALL --build R
+R CMD INSTALL --build R sqlmlutils.tar.gz
 mv sqlmlutils_*.zip R/dist
+mv sqlmlutils_*.tar.gz R/dist
 popd
