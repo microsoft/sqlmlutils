7125e0d962214f7c634c95744ed0ca281e441363
Jonathan Zhu <joz@microsoft.com>
2020-07-14 14:32:55 -0700

    fix conftest

--- Python/tests/conftest.py
+++ Python/tests/conftest.py
@@ -2,10 +2,13 @@
 # Licensed under the MIT license.
 
 import os
+from sys import platform
 
 from sqlmlutils import ConnectionInfo, Scope
 
-driver = os.environ['DRIVER'] if 'DRIVER' in os.environ else "SQL Server"
+default_driver = "ODBC Driver 17 for SQL Server" if platform != "win32" else "SQL Server
+
+driver = os.environ['DRIVER'] if 'DRIVER' in os.environ else default_driver
 server = os.environ['SERVER'] if 'SERVER' in os.environ else "localhost"
 database = os.environ['DATABASE'] if 'DATABASE' in os.environ else "AirlineTestDB"
 uid = os.environ['USER'] if 'USER' in os.environ else ""
