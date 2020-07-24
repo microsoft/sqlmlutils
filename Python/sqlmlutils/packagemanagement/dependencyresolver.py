54a6eb98ccd5d48febff28396f46ca4c3506331f
Jonathan Zhu <joz@microsoft.com>
2020-07-23 18:26:57 -0700

    dependency resolution

--- Python/sqlmlutils/packagemanagement/dependencyresolver.py
+++ Python/sqlmlutils/packagemanagement/dependencyresolver.py
@@ -3,6 +3,7 @@
 
 import operator
 
+from pkg_resources import Requirement
 from distutils.version import LooseVersion
 
 class DependencyResolver:
@@ -28,9 +29,8 @@ class DependencyResolver:
         for requirement in target_requirements:
             reqmet = self._package_exists_on_server(requirement.name)
 
-            for spec in requirement.specs:
-                reqmet = reqmet & self._check_if_installed_package_meets_spec(
-                    self._server_packages, requirement.name, spec)
+            reqmet = reqmet & self._check_if_installed_package_meets_spec(
+                     self._server_packages, requirement)
 
             if not reqmet or requirement.name == self._target_package:
                 required_packages.append(self.clean_requirement_name(requirement.name))
@@ -41,24 +41,23 @@ class DependencyResolver:
                     self.clean_requirement_name(serverpkg[0].lower())
                     for serverpkg in self._server_packages])
 
+    def _check_package_meets_req(self, requirement):
+        pkgs = self._server_packages
+        req = Requirement.parse(requirement)
+        
+
     @staticmethod
     def clean_requirement_name(reqname: str):
         return reqname.replace("-", "_")
 
     @staticmethod
-    def _check_if_installed_package_meets_spec(package_tuples, name, spec):
-        op_str = spec[0]
-        req_version = spec[1]
-
+    def _check_if_installed_package_meets_spec(package_tuples, requirement):
         installed_package_name_and_version = [package for package in package_tuples \
-            if DependencyResolver.clean_requirement_name(name.lower()) == \
+            if DependencyResolver.clean_requirement_name(requirement.name.lower()) == \
                 DependencyResolver.clean_requirement_name(package[0].lower())]
             
         if not installed_package_name_and_version:
             return False
 
-        installed_package_name_and_version = installed_package_name_and_version[0]
-        installed_version = installed_package_name_and_version[1]
-
-        operator_map = {'>': 'gt', '>=': 'ge', '<': 'lt', '==': 'eq', '<=': 'le', '!=': 'ne'}
-        return getattr(operator, operator_map[op_str])(LooseVersion(installed_version), LooseVersion(req_version))
+        installed_version = installed_package_name_and_version[0][1]
+        return Requirement.parse(requirement.line).specifier.contains(installed_version)
