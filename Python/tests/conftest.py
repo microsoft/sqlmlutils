a564e14903c28373ba9b84c36fc28cce120514b4
Jonathan Zhu <joz@microsoft.com>
2020-10-08 19:05:56 -0700

    revert

--- Python/tests/conftest.py
+++ Python/tests/conftest.py
@@ -6,13 +6,13 @@ import os
 from sqlmlutils import ConnectionInfo, Scope
 
 driver = os.environ['DRIVER'] if 'DRIVER' in os.environ else "SQL Server"
-server = os.environ['SERVER'] if 'SERVER' in os.environ else "sqlmlutils-mi-server.public.0c3d2187285f.database.windows.net,3342"
+server = os.environ['SERVER'] if 'SERVER' in os.environ else "localhost"
 database = os.environ['DATABASE'] if 'DATABASE' in os.environ else "AirlineTestDB"
-uid = os.environ['USER'] if 'USER' in os.environ else "extensibilityuser"
-pwd = os.environ['PASSWORD'] if 'PASSWORD' in os.environ else "cereal-whizzing-salute-900!@#"
+uid = os.environ['USER'] if 'USER' in os.environ else ""
+pwd = os.environ['PASSWORD'] if 'PASSWORD' in os.environ else ""
 
 uidAirlineUser = "AirlineUserdbowner"
-pwdAirlineUser = os.environ['PASSWORD_AIRLINE_USER'] if 'PASSWORD_AIRLINE_USER' in os.environ else "testing!123"
+pwdAirlineUser = os.environ['PASSWORD_AIRLINE_USER'] if 'PASSWORD_AIRLINE_USER' in os.environ else "FakeT3sterPwd!"
 
 scope = Scope.public_scope() if uid == "" else Scope.private_scope()
 
