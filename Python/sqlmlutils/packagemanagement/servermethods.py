ae520fdaddfe57ca9e9e9d270cc3c22a5e615546
Jonathan Zhu <joz@microsoft.com>
2020-10-12 18:06:36 -0700

    max wheel version and sysconfig

--- Python/sqlmlutils/packagemanagement/servermethods.py
+++ Python/sqlmlutils/packagemanagement/servermethods.py
@@ -15,16 +15,20 @@ def show_installed_packages():
 
 def get_server_info():
     from distutils.version import LooseVersion
-    import pip
-    if LooseVersion(pip.__version__) > LooseVersion("10"):
+    import pip, sysconfig
+    pipversion = LooseVersion(pip.__version__)
+
+    if pipversion >= LooseVersion("19.3"):
+        from wheel import pep425tags
+    elif pipversion > LooseVersion("10"):
         from pip._internal import pep425tags
     else:
         from pip import pep425tags
     return {
-        "impl_version_info": pep425tags.get_impl_version_info(),
-        "abbr_impl": pep425tags.get_abbr_impl(),
-        "abi_tag": pep425tags.get_abi_tag(),
-        "platform": pep425tags.get_platform()
+        "impl_version_info": pep425tags.get_impl_version_info(), #(3,7)
+        "abbr_impl": pep425tags.get_abbr_impl(), #'cp'
+        "abi_tag": pep425tags.get_abi_tag(), #'cp37m'
+        "platform": sysconfig.get_platform().replace("-","_") #'win_amd64', 'linux_x86_64'
     }
 
 
