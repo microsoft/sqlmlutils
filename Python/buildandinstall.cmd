011340debb47c7731d0a42d883072788b9cdd3a2
Jonathan Zhu <joz@microsoft.com>
2020-10-19 10:15:11 -0700

    Add links for dist

--- Python/buildandinstall.cmd
+++ Python/buildandinstall.cmd
@@ -1,3 +1,4 @@
 del /q dist\*
 python.exe setup.py sdist --formats=zip
+python.exe setup.py bdist_wheel
 python.exe -m pip install --upgrade --upgrade-strategy only-if-needed --find-links=dist sqlmlutils
